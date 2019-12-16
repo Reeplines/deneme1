9bd81eefb340282f5c780beeda4a09bc0622f7f0
Reeplines <bmtfebubekir@gmail.com>
2019-12-16 08:13:36 +0300

    ilk githuba projem

---
+++ Çarpma işlemini kullanmadan iki sayının çarpımını bulan c programı.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+	int f,s,t,tekrar;
+	
+	do{
+	
+	cout<<"birinci sayiyi giriniz ";
+	cin>>f;
+	cout<<"ikinci sayiyi giriniz ";
+	cin>>s;
+	
+	for (int i=1;i<=f;i++)
+	{
+	
+	t=t+s;
+	if (i==f)
+	cout<<"carpimin sonucu = "<<t<<"\n\n";
+	
+	
+	
+}
+t=0;	
+}while(tekrar==0);
+	return 0;
+}
